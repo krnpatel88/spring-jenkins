8887c9943e3f61fb23125e2aa52f38d803db31f7
kiran <krnpatel88@gmail.com>
2022-03-08 19:22:46 +0530

    Fourth commit

--- src/main/java/com/example/demo/SpringJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -19,7 +19,7 @@ public class SpringJenkinsApplication {
 	}
 	
 	public static void main(String[] args) {
-		logger.info("Application executed..***************************");
+		logger.info("Application executed..**************************with pipeline*");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
